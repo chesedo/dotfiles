c4ca50a907154a1da1ed2cb408223b14eeb5fd60
chesedo <pieter@chesedo.me>
2021-07-10 08:47:53 +0200

    Update install scripts

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
 # Install
-sudo apt install -y zsh fonts-powerline
-chsh -s $(which zsh)
 ln -sf $PWD/.zshrc ~/.zshrc
 rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 
+ZSH_CUSTOM=$ZSH/custom
+
 # Setups
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  echo "zsh-autosuggestions already exists"
+else
+  git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+fi
+
+if [ -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  echo "zsh-syntax-highlighting already exists"
+else
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
 
 # Install powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+if [ -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  echo "powerlevel10k already exists"
+else
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+fi
+
 ln -sf $PWD/.p10k.zsh ~/.p10k.zsh
 
-# Instal fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
+# # Instal fzf
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install --all
--- setup.sh
+++ setup.sh
@@ -2,12 +2,17 @@
 
 sudo apt update
 
-# Install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+# Install
+
+# # Install fzf
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+# ~/.fzf/install
 
 # Install zsh
+sudo apt install -y zsh fonts-powerline
+chsh -s $(which zsh)
 ./setup-zsh.sh
 
-# Install nvim
+# Install and setup nvim
+sudo apt install -y neovim
 ./setup-nvim.sh
