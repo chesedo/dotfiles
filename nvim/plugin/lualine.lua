58364402efd9a3f50c8d2322db3448a3c62a3cc3
chesedo <pieter@chesedo.me>
2021-07-25 15:32:50 +0200

    Add diagnostics to lualine

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -1,5 +1,13 @@
 require('lualine').setup{
   options = {
     theme = 'nord'
+  },
+  sections = {
+    lualine_x = {
+      { 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
+      'encoding',
+      'fileformat',
+      'filetype',
+    }
   }
 }
