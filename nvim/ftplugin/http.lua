5daf49520d270ef25c55e781f0494afe469e26f5
chesedo <pieter@chesedo.me>
2021-08-10 20:34:18 +0200

    Remove buf from mapping

--- nvim/ftplugin/http.lua
+++ nvim/ftplugin/http.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>c', '<cmd>lua require("rest-nvim").run()<CR>', { noremap=true, silent=true })
+vim.api.nvim_set_keymap('n', '<leader>c', '<cmd>lua require("rest-nvim").run()<CR>', { noremap=true, silent=true })
