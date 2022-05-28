c33fa47eb26a6008c705b79a31df85114c060094
chesedo <pieter@chesedo.me>
2022-05-28 18:23:35 +0200

    feat: enable printing

--- nixos/configuration-b450.nix
+++ nixos/configuration-b450.nix
@@ -27,9 +27,17 @@
   };
 
   programs.steam.enable = true;
+  programs.system-config-printer.enable = true;
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services = {
+    avahi.enable = true;
+
+    printing = {
+      enable = true;
+      drivers = [ pkgs.canon-cups-ufr2 pkgs.carps-cups ];
+    };
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
