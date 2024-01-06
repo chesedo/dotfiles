352afeb645046301b1ec485e142da4412f8868d5
chesedo <pieter@chesedo.me>
2024-01-06 16:09:42 +0000

    feat: single screen overview with events

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
 { pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
+let
+  my-python-packages = ps:
+    with ps; [
+      google-api-python-client
+      google-auth-oauthlib
+    ];
+in pkgs.mkShell {
   buildInputs = with pkgs; [ nixfmt ];
+  packages = [ (pkgs.python3.withPackages my-python-packages) ];
 }
