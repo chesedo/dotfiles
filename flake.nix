fedb27884042397c1893a5d2edd4cc331bb1f493
chesedo <pieter@chesedo.me>
2025-04-30 16:44:38 +0200

    Only build for x86_64

--- flake.nix
+++ flake.nix
@@ -3,14 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default =
-          pkgs.mkShell { buildInputs = with pkgs; [ nixfmt-rfc-style ]; };
-      });
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nixfmt-rfc-style
+          nil
+        ];
+      };
+    };
 }
