9122c874f1e4787aa440c74120372da74fa11cd7
chesedo <pieter@chesedo.me>
2025-05-05 10:49:04 +0200

    Reduce cursor size

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -400,7 +400,7 @@ in
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
-    size = 24;
+    size = 20;
     gtk.enable = true;
     x11.enable = true;
   };
