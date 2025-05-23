748536c9f16153e8fba38b4e9664aa03141feec0
chesedo <pieter@chesedo.me>
2025-05-23 12:17:57 +0200

    Use amber cursor instead

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -410,7 +410,7 @@ in
 
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Ice";
+    name = "Bibata-Modern-Amber";
     size = 20;
     gtk.enable = true;
     x11.enable = true;
