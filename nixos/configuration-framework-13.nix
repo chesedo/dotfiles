923f401d67e556fb06d81ce4222d81791e84267c
chesedo <pieter@chesedo.me>
2026-05-28 13:11:50 +0200

    Update state versions and remove fwupd service
    Update home-manager state version from 22.11 to 25.11 and system state
    version from 24.11 to 25.11. Remove explicit fwupd service configuration
    from framework-13 configuration since it is auto set now.

--- nixos/configuration-framework-13.nix
+++ nixos/configuration-framework-13.nix
@@ -32,8 +32,6 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
-  services.fwupd.enable = true;
-
   # Enable sound with pipewire.
   services.pipewire = {
     enable = true;
@@ -54,7 +52,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 
   system.userActivationScripts.linktosharedfolders.text = ''
     if [[ ! -h "$HOME/git" ]]; then
